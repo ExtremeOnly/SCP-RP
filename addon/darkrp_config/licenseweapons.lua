7ca0bb84e73c0955a56439617a7e5cd637f82730
Zabójca997 <31369842+Zabujca997@users.noreply.github.com>
2020-11-26 11:24:40 +0100

    Add files via upload

---
+++ addon/darkrp_config/licenseweapons.lua
@@ -0,0 +1,12 @@
+--[[---------------------------------------------------------------------------
+License weapons
+Add weapons that do NOT require a special license here
+ALL other weapons will require a license
+
+Note: this only works if the license setting is enabled
+---------------------------------------------------------------------------]]
+GM.NoLicense["cw_makarov"] = true
+GM.NoLicense["cw_p99"] = true
+GM.NoLicense["cw_m1911"] = true
+GM.NoLicense["cw_fiveseven"] = true
+GM.NoLicense["weapon_stunstick"] = true
